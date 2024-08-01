ec668dd5c386d3a7aa95a7cdd6f12b44c2b34039
skalsduq52 <skalsduq@naver.com>
2024-08-01 14:40:23 +0900

    new

---
+++ src/main/java/com/yeop/Laplace/LaplaceApplication.java
@@ -0,0 +1,13 @@
+package com.yeop.Laplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeop/Laplace/LaplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeop.Laplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

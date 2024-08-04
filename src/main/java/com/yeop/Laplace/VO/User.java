1c01441ce2357ef0c0c7b9a70f14582c0e714210
skalsduq52 <skalsduq@naver.com>
2024-08-04 23:39:33 +0900

    new

--- src/main/java/com/yeop/Laplace/VO/User.java
+++ src/main/java/com/yeop/Laplace/VO/User.java
@@ -1,12 +1,26 @@
 package com.yeop.Laplace.VO;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "USER")
+
 public class User {
-    private String id;
+    @Id
+    @Column(name = "USERID")
+    private String userId;
+
+    @Column(name = "NAME", nullable = false)
     private String name;
+
+    @Column(name = "AGE", nullable = false)
+    private int age;
 
 }

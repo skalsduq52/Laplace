e9277b4a204efc7372fd282555925b3a685d6b3b
skalsduq52 <skalsduq@naver.com>
2024-08-06 17:25:34 +0900

    Swagger API 연동

--- src/main/java/com/yeop/Laplace/controller/user/UserController.java
+++ src/main/java/com/yeop/Laplace/controller/user/UserController.java
@@ -3,6 +3,8 @@ package com.yeop.Laplace.controller.user;
 import com.yeop.Laplace.VO.User;
 import com.yeop.Laplace.repository.UserRepository;
 import com.yeop.Laplace.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/users")
+@Tag(name = "유저", description = "유저 관련 api 입니다.")
 @RestController
 public class UserController {
 
@@ -18,20 +21,28 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("/create")
+    @Tag(name = "User API")
+    @Operation(summary = "User 생성", description = "User 정보를 생성합니다.")
     public ResponseEntity<?> createUser(User user){
         return ResponseEntity.ok(userRepository.save(user));
     }
 
     @GetMapping("/lists")
+    @Tag(name = "User API")
+    @Operation(summary = "User 목록 조회", description = "전체 User 정보를 조회합니다.")
     public List<User> getUsers(){
         return userRepository.findAll();
     }
 
+    @Tag(name = "User API")
+    @Operation(summary = "User 정보 조회", description = "특정 User 정보를 조회합니다.")
     @GetMapping("/lists/{userId}")
     public User getUser(@PathVariable String userId) {
         return userRepository.findByUserId(userId);
     }
 
+    @Tag(name = "User API")
+    @Operation(summary = "User 삭제", description = "특정 User를 삭제 합니다.")
     @DeleteMapping("/delete/{userId}")
     @Transactional
     public void deleteUser(@PathVariable String userId) {
---
+++ src/main/java/com/yeop/Laplace/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.yeop.Laplace.swagger;
+
+import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("springdoc-openapi")
+                        .version("1.0")
+                        .description("springdoc-openapi swagger-ui 화면입니다."));
+    }
+
+    @Bean
+    public GroupedOpenApi api() {
+        String[] paths = {"/**"};
+        String[] packagesToScan = {"com.yeop.Laplace"};
+        return GroupedOpenApi.builder().group("springdoc-openapi")
+                .pathsToMatch(paths)
+                .packagesToScan(packagesToScan)
+                .build();
+    }
+}

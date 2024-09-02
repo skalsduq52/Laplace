ee828ec7756bf86a1018ad15dd911a2e29d3fa28
skalsduq52 <skalsduq@naver.com>
2024-09-03 00:21:44 +0900

    리액트 페이지 구성 및 캐릭터 검색 API

--- src/main/java/com/yeop/Laplace/controller/LostArkController.java
+++ src/main/java/com/yeop/Laplace/controller/LostArkController.java
@@ -23,6 +23,11 @@ public class LostArkController {
 
     static final String apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDA1NDc3MDcifQ.mo9WzWy4tGvl9Ih2Ulv7-M3xRLzbY56zNvkN13LC-9kQmOjvIRTEgyso_7Gezk8jKwcif-R_7T6h4odGRhtzrpTRYSJU4lwYFN9k_vORisYK5rJomY6EGfB7Igep8sHl41TVP6m3BWNQJ2NYVVpDEHrUzXmpidXj2a1uFOCfwhCeCtx9VG3du4Dqa9C4yTFfYmB0AS7TcE9a6oeveYbe7IVElVY9StLvBNiodYGY6KdaUXM6_nBegCEDPZCyLZNovjO45IAfIhsPAkzpTD6FCQzz30DaekF6DO-WhemqfxHt-RbuxVC0ZjEU90IaLleHRMX7deuqjBEjQ4BxZXrlUw";
 
+    @PostMapping("/loa")
+    public void test(){
+        System.out.println("hello");
+    }
+
     @GetMapping("")
     public Character getUser(){
         String url = "https://developer-lostark.game.onstove.com/armories/characters/%EB%82%98%EB%AF%BC%EC%97%BD%EC%B4%9D/profiles";
--- src/main/lostark/src/App.js
+++ src/main/lostark/src/App.js
@@ -1,23 +1,55 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+
+function test(characterName) {
+    const req = {
+        character : characterName
+    };
+
+    fetch("http://127.0.0.1:8080/loa)",{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+    console.log("req");
+    console.log(JSON.stringify(req));
+}
 
 function App() {
+    const [inputValue, setInputValue] = useState('');
+    const handleClick = () => {
+        test(inputValue)
+    }
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="top-header">
+        <div className="container">
+          <a className="logo-home" href="http://localhost:3000">민엽 군장검사</a>
+          <input className="search-cha" placeholder="캐릭터 명을 입력하세요" value={inputValue} onChange={handleChange} onKeyPress={handleClick}></input>
+            <button className="chbutton" onClick={handleClick} >ㅎㅇ</button>
+        </div>
       </header>
+      <nav className="top-nav">
+        <div className="container">
+          <ul className="navigation">
+            <li>홈</li>
+            <li>골드 정보</li>
+            <li>임시</li>
+            <li>임시2</li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
